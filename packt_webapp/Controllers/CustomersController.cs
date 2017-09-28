c16aba8250709bc9870021aef2ddbacd3b86b157
tedonmain <tedonmain@gmail.com>
2017-09-28 14:47:09 -0700

    logging, etc

--- packt_webapp/Controllers/CustomersController.cs
+++ packt_webapp/Controllers/CustomersController.cs
@@ -8,27 +8,105 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace packt_webapp.Controllers
 {
     [Route("api/[controller]")]
     public class CustomersController : Controller
     {
+        private readonly ILogger<CustomersController> _logger;
+
         private ICustomerRepository _customerRepository;
 
-        public CustomersController(ICustomerRepository customerRepository)
+        public CustomersController(ICustomerRepository customerRepository, ILogger<CustomersController> logger)
         {
             _customerRepository = customerRepository;
+            _logger = logger;
+            _logger.LogInformation(" ------> CustomersControler started...");
+
         }
 
         [HttpGet]
-		public IActionResult GetAllCustomers()
-		{
-			var allCustomers = _customerRepository.GetAll().ToList();
+        public IActionResult GetAllCustomers()
+        {
+            //throw new Exception("  ----> Test Exception");
 
-			var allCustomersDto = allCustomers.Select(x => Mapper.Map<CustomerDto>(x));
+            _logger.LogInformation(" -------> GetAllCustomers()...");
 
-			return Ok(allCustomersDto);
-		}
+            var allCustomers = _customerRepository.GetAll().ToList();
+
+            var allCustomersDto = allCustomers.Select(x => Mapper.Map<CustomerDto>(x));
+
+            return Ok(allCustomersDto);
+        }
+
+        [HttpGet]
+        [Route("{id}", Name = "GetSingleCustomer")]
+        public IActionResult GetSingleCustomer(Guid id)
+        {
+            Customer CustomerFromRepo = _customerRepository.GetSingle(id);
+            if (CustomerFromRepo == null)
+            {
+                return NotFound();
+            }
+            return Ok(Mapper.Map<CustomerDto>(CustomerFromRepo));
+        }
+
+        // POST api/customers - adding a new customer
+        [HttpPost]
+        public IActionResult AddCustomer([FromBody]CustomerCreateDto customerCreateDto)
+        {
+            Customer toAdd = Mapper.Map<Customer>(customerCreateDto);
+            _customerRepository.Add(toAdd);
+            bool result = _customerRepository.Save();
+            if (!result) throw new Exception("(POST):something went wrong when adding a new customer");
+            //return Ok(Mapper.Map<CustomerDto>(toAdd));
+            return CreatedAtRoute("GetSingleCustomer", new { id = toAdd.Id }, Mapper.Map<CustomerDto>(toAdd));
+        }
+
+        // PUT api/customers/id 
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult UpdateCustomer(Guid id, [FromBody] CustomerUpdateDto updateDto)
+        {
+            var existingCustomer = _customerRepository.GetSingle(id);
+            if (existingCustomer == null)
+            {
+                return NotFound();
+            }
+            Mapper.Map(updateDto, existingCustomer);
+            _customerRepository.Update(existingCustomer);
+            bool result = _customerRepository.Save();
+            if (!result) throw new Exception($"(PUT):something went wrong when updating the customer with id: {id}");
+			return Ok(Mapper.Map<CustomerDto>(existingCustomer));
+        }
+
+        [HttpPatch]
+        [Route("{id}")]
+        public IActionResult PartiallyUpdate(Guid id, [FromBody] JsonPatchDocument<CustomerUpdateDto> customerPatchDoc)
+        {
+            if (customerPatchDoc == null) return BadRequest();
+            var existingCustomer = _customerRepository.GetSingle(id);
+            if (existingCustomer == null) return NotFound();
+            var customerToPatch = Mapper.Map<CustomerUpdateDto>(existingCustomer);
+            customerPatchDoc.ApplyTo(customerToPatch);
+            Mapper.Map(customerToPatch, existingCustomer);
+            _customerRepository.Update(existingCustomer);
+            bool result = _customerRepository.Save();
+            if (!result) throw new Exception($"(PATCH):something went wrong when updating the customer with id: {id}");
+            return Ok(Mapper.Map<CustomerDto>(existingCustomer));
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Remove(Guid id) {
+            var existingCustomer = _customerRepository.GetSingle(id);
+            if (existingCustomer == null) return NotFound();
+            _customerRepository.Delete(id);
+            bool result = _customerRepository.Save();
+            if (!result) throw new Exception($"(DELETE):something went wrong when deleting the customer with id: {id}");
+            return NoContent();
+        }
     }
 }
--- packt_webapp/Startup.cs
+++ packt_webapp/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using packt_webapp.Middlewares;
 using System.IO;
 using System.Diagnostics;
@@ -18,6 +19,10 @@ using packt_webapp.Entities;
 using packt_webapp.Repositories;
 using packt_webapp.Dtos;
 using packt_webapp.Services;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using NLog.Web;
+using NLog.Extensions.Logging;
+using Microsoft.AspNetCore.Diagnostics;
 
 namespace packt_webapp
 {
@@ -32,11 +37,14 @@ namespace packt_webapp
     {
         public IConfigurationRoot Configuration { get; }
 
-        public Startup(IHostingEnvironment env) {
-			var builder = new ConfigurationBuilder()
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
                 //.SetBasePath(Directory.GetCurrentDirectory())
-				.SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json");
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+            env.ConfigureNLog("nlog.config");
 
             Configuration = builder.Build();
 
@@ -45,34 +53,67 @@ namespace packt_webapp
 
         }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-		public void ConfigureServices(IServiceCollection services)
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
         {
             services.AddOptions();
 
             //services.Configure<MyConfiguration>(Configuration);
 
-            services.AddDbContext<PacktDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            //services.AddDbContext<PacktDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContextPool<PacktDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddScoped<ICustomerRepository, CustomerRepository>();
 
             services.AddScoped<ISeedDataService, SeedDataService>();
 
-            services.AddMvc();
+            services.AddMvc(config =>
+            {
+                config.ReturnHttpNotAcceptable = true;
+                config.OutputFormatters.Add(new XmlSerializerOutputFormatter());
+                config.InputFormatters.Add(new XmlSerializerInputFormatter());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug(LogLevel.Error);
+
+            //nlog
+            loggerFactory.AddNLog();
+            // add NLog Web
+            app.AddNLogWeb();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler(errorApp =>
+                {
+                    errorApp.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        context.Response.ContentType = "text/plain";
+                        var errorFeature = context.Features.Get<IExceptionHandlerFeature>();
+                        if (errorFeature != null)
+                        {
+                            var logger = loggerFactory.CreateLogger("Global exception logger");
+                            logger.LogError(500, errorFeature.Error, errorFeature.Error.Message);
+                        }
+
+                        await context.Response.WriteAsync("There was an error, Bunky");
+                    });
+                });
+            }
 
             if (env.IsEnvironment("MyEnvironment"))
-			{
-				app.UseCustomMiddleware();
+            {
+                app.UseCustomMiddleware();
             }
 
             // use index.html instead
@@ -85,11 +126,17 @@ namespace packt_webapp
             app.UseStaticFiles();
 
 
-            AutoMapper.Mapper.Initialize(mapper => { mapper.CreateMap<Customer, CustomerDto>().ReverseMap(); });
+            AutoMapper.Mapper.Initialize(mapper =>
+            {
+                mapper.CreateMap<Customer, CustomerDto>().ReverseMap();
+                mapper.CreateMap<Customer, CustomerCreateDto>().ReverseMap();
+                mapper.CreateMap<Customer, CustomerUpdateDto>().ReverseMap();
+            });
+
 
             //app.UseMiddleware<CustomMiddleware>();
             //app.UseCustomMiddleware();
-             
+
             app.AddSeedData();
 
             app.UseMvc();
